b95afe68ef8bcc903cb0455daab72a800460f9bd
Martin Polden <martin.polden@gmail.com>
2011-02-28 19:00:48 +0100

    Updated POM

--- code/server/src/main/java/no/kantega/server/model/TransactionResource.java
+++ code/server/src/main/java/no/kantega/server/model/TransactionResource.java
@@ -1,6 +1,5 @@
 package no.kantega.server.model;
 
-import com.sun.corba.se.pept.transport.ContactInfo;
 import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
 import org.restlet.resource.Put;
@@ -11,11 +10,8 @@ public interface TransactionResource {
 
     @Get
     public List<Transaction> retrieve();
-
     @Put
     public void store(Transaction transaction);
-
     @Delete
     public void remove();
-
 }
