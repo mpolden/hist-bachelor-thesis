25a1d78eb91330fb6771a99a75f4ac709361dbd8
Vegard <vegardoj@vegardoj-asus.(none)>
2011-02-28 16:32:44 +0100

    Created tablayout with two tabs: overview and synchronize

--- code/android/src/no/kantega/android/MenuActivity.java
+++ code/android/src/no/kantega/android/MenuActivity.java
@@ -1,13 +1,38 @@
 package no.kantega.android;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MenuActivity extends Activity {
-    /** Called when the activity is first created. */
+public class MenuActivity extends TabActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);        
+        setupTabs();
+        
+    }
+    
+    private void setupTabs() {
+    	Resources res = getResources();
+    	TabHost tabHost = getTabHost();
+    	
+    	TabHost.TabSpec spec;
+    	Intent intent;
+    	intent = new Intent().setClass(this, OverviewActivity.class);
+    	String overview = res.getString(R.string.overview);
+    	spec = tabHost.newTabSpec(overview).setIndicator(overview, res.getDrawable(R.drawable.tab_overview)).setContent(intent);
+    	tabHost.addTab(spec);
+    	
+    	intent = new Intent().setClass(this, SynchronizeActivity.class);
+    	String synchronize = res.getString(R.string.synchronize);
+    	spec = tabHost.newTabSpec(synchronize).setIndicator(synchronize, res.getDrawable(R.drawable.tab_synchronize)).setContent(intent);
+    	tabHost.addTab(spec);
+    	
+    	tabHost.setCurrentTab(0);
+    	
     }
 }
