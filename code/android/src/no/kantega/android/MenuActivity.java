98bed25ba9ddf96325f8934ddd084ea8209ac2cc
Vegard <vegardoj@vegardoj-asus.(none)>
2011-02-28 15:07:53 +0100

    Created project

--- code/android/src/no/kantega/android/MenuActivity.java
+++ code/android/src/no/kantega/android/MenuActivity.java
@@ -1,27 +1,13 @@
 package no.kantega.android;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MenuActivity extends Activity {
-
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.menu);
-        TextView textView = (TextView) findViewById(R.id.placeholder);
-        textView.setText(R.string.description);
-        Button overview = (Button) findViewById(R.id.overview);
-        overview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),
-                        OverviewActivity.class));
-            }
-        });
+        setContentView(R.layout.main);
     }
 }
