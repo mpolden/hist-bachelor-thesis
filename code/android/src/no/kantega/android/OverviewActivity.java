38ce999e2ea984c9aff0231ebc87ddefd61a2f54
Vegard <vegardoj@vegardoj-asus.(none)>
2011-02-28 16:34:22 +0100

    Created a simple layout with some drawables

---
+++ code/android/src/no/kantega/android/OverviewActivity.java
@@ -0,0 +1,13 @@
+package no.kantega.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OverviewActivity extends Activity {
+	
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.overview);
+	    }
+}
---
+++ code/android/src/no/kantega/android/SynchronizeActivity.java
@@ -0,0 +1,28 @@
+package no.kantega.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class SynchronizeActivity extends Activity {	
+		
+	private OnClickListener syncButtonListener = new OnClickListener() {		
+		@Override
+		public void onClick(View v) {
+			CharSequence text = "Synchronized";			
+			Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();		
+		}
+	};
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.synchronize);
+	    ImageButton syncButton = (ImageButton)findViewById(R.id.syncButton);
+	    syncButton.setImageResource(R.drawable.syncbutton);
+	    syncButton.setOnClickListener(syncButtonListener);
+	}
+}
