9a2fd85f5c6c586c031905c71234aa955fb6ffb8
Martin Polden <martin.polden@gmail.com>
2011-03-01 15:16:07 +0100

    JPQL rage

--- code/server/app/Import.java
+++ code/server/app/Import.java
@@ -10,7 +10,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@OnApplicationStart
 public class Import extends Job {
 
     public void doJob() {
--- code/server/app/controllers/Transactions.java
+++ code/server/app/controllers/Transactions.java
@@ -1,16 +1,27 @@
 package controllers;
 
 import play.*;
+import play.db.jpa.JPA;
 import play.mvc.*;
 
 import java.util.*;
 
 import models.*;
 
+import javax.persistence.Query;
+
 public class Transactions extends Controller {
 
     public static void topTags(int count) {
-        //Transaction.find("")
+        //List<TransactionTag> tags = TransactionTag.find("")
+        Query query = JPA.em().createQuery(
+                "select tag.name, sum(t.amountOut) from Transaction t join t.tags as tag" +
+                        " group by tag.name");
+        List l = query.getResultList();
+        for (Object o : l) {
+            renderText(o);
+        }
+        //renderJSON(tags);
     }
 
     public static void transactions() {
