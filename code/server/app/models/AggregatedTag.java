563566238c79b3fd0b41a9070fbe2095542f320a
Martin Polden <martin.polden@gmail.com>
2011-03-02 13:54:09 +0100

    Moved aggregated data to models

---
+++ code/server/app/models/AggregatedTag.java
@@ -0,0 +1,23 @@
+package models;
+
+public class AggregatedTag {
+
+    private String name;
+    private Double amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
