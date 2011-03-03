bca01994f2d0840b4982bcf7156e845c5243cda8
Martin Polden <martin.polden@gmail.com>
2011-03-03 15:02:17 +0100

    Use static date when testing date formatting

--- code/server/test/GsonUtilTest.java
+++ code/server/test/GsonUtilTest.java
@@ -2,19 +2,15 @@ import org.junit.Test;
 import play.test.UnitTest;
 import utils.GsonUtil;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class GsonUtilTest extends UnitTest {
 
     @Test
     public void testRenderJSONWithDateFmt() {
-        final Date now = new Date();
-        final SimpleDateFormat sdf = new SimpleDateFormat(
-                "yyyy-MM-dd HH:mm:ss");
-        String expected = String.format("\"%s\"", sdf.format(now));
+        String expected = "\"2010-01-01 00:00:00\"";
         String actual = GsonUtil.renderJSONWithDateFmt("yyyy-MM-dd HH:mm:ss",
-                now);
+                new Date(1262300400000L));
         assertEquals(expected, actual);
     }
 }
