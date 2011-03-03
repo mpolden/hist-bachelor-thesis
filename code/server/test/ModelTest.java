2ddb8f16659725450949bc1655446f26903b4ea0
Martin Polden <martin.polden@gmail.com>
2011-03-03 14:34:16 +0100

    Added tests for import, model and routes

---
+++ code/server/test/ModelTest.java
@@ -0,0 +1,34 @@
+import models.Transaction;
+import models.TransactionTag;
+import models.TransactionType;
+import org.junit.Test;
+import play.test.UnitTest;
+
+public class ModelTest extends UnitTest {
+
+    @Test
+    public void testTransactionFields() {
+        Transaction t = Transaction.findById(1L);
+        assertNotNull(t.accountingDate);
+        assertNotNull(t.fixedDate);
+        assertNotNull(t.amountIn);
+        assertNotNull(t.amountOut);
+        assertNotNull(t.text);
+        assertNotNull(t.archiveRef);
+        assertNotNull(t.type);
+        assertNotNull(t.tags);
+        assertTrue(t.tags.size() > 0);
+    }
+
+    @Test
+    public void testTransactionTagFields() {
+        TransactionTag t = TransactionTag.findById(1L);
+        assertNotNull(t.name);
+    }
+
+    @Test
+    public void testTransactionTypeFields() {
+        TransactionType t = TransactionType.findById(1L);
+        assertNotNull(t.name);
+    }
+}
