45dbad64e186936f2666393ff9eea152a9d5869d
Martin Polden <martin.polden@gmail.com>
2011-03-07 10:46:24 +0100

    Added equals and hashCode

--- code/android/src/no/kantega/android/models/TransactionTag.java
+++ code/android/src/no/kantega/android/models/TransactionTag.java
@@ -3,4 +3,19 @@ package no.kantega.android.models;
 public class TransactionTag {
 
     public String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TransactionTag)) return false;
+        TransactionTag that = (TransactionTag) o;
+        if (name != null ? !name.equals(that.name) : that.name != null)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
--- code/android/src/no/kantega/android/models/TransactionType.java
+++ code/android/src/no/kantega/android/models/TransactionType.java
@@ -3,4 +3,19 @@ package no.kantega.android.models;
 public class TransactionType {
 
     public String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TransactionType)) return false;
+        TransactionType that = (TransactionType) o;
+        if (name != null ? !name.equals(that.name) : that.name != null)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
