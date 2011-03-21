ded04e26a8451b7bf99e44031d9bea6fe20e0277
Martin Polden <martin.polden@gmail.com>
2011-03-21 18:01:26 +0100

    Updated server tests

--- code/server/test/RouteTest.java
+++ code/server/test/RouteTest.java
@@ -14,26 +14,8 @@ public class RouteTest extends FunctionalTest {
     }
 
     @Test
-    public void testRouteAvg() {
-        Response response = GET("/t/avg");
-        assertIsOk(response);
-        assertContentType("application/json", response);
-        assertCharset("utf-8", response);
-        assertTrue(getContent(response).length() > 0);
-    }
-
-    @Test
-    public void testRouteTags() {
-        Response response = GET("/t/tags/1");
-        assertIsOk(response);
-        assertContentType("application/json", response);
-        assertCharset("utf-8", response);
-        assertTrue(getContent(response).length() > 0);
-    }
-
-    @Test
     public void testRouteTransactions() {
-        Response response = GET("/t/transactions");
+        Response response = GET("/transactions");
         assertIsOk(response);
         assertContentType("application/json", response);
         assertCharset("utf-8", response);
@@ -41,8 +23,8 @@ public class RouteTest extends FunctionalTest {
     }
 
     @Test
-    public void testRouteFresh() {
-        Response response = GET("/t/fresh/0");
+    public void testRouteTransactionsAfter() {
+        Response response = GET("/transactions/0");
         assertIsOk(response);
         assertContentType("application/json", response);
         assertCharset("utf-8", response);
@@ -55,7 +37,7 @@ public class RouteTest extends FunctionalTest {
                 "\"fixedDate\":\"2011-02-17 00:00:00\",\"amountIn\":0.0,\"amountOut\":1321.0,\"text\":\"test\"," +
                 "\"internal\":true,\"timestamp\":1300379994253,\"dirty\":true,\"type\":{\"name\":\"Kontant\",\"id\":6}," +
                 "\"tag\":{\"name\":\"Bil\",\"id\":5},\"id\":0}]";
-        Response response = POST("/t/save", "application/json", json);
+        Response response = POST("/transactions", "application/json", json);
         assertIsOk(response);
         assertContentType("application/json", response);
         assertCharset("utf-8", response);
@@ -69,7 +51,7 @@ public class RouteTest extends FunctionalTest {
                 "\"fixedDate\":\"2011-02-17 00:00:00\",\"amountIn\":0.0,\"amountOut\":1321.0,\"text\":\"test\"," +
                 "\"internal\":true,\"timestamp\":1300379994253,\"dirty\":false,\"type\":{\"name\":\"Kontant\",\"id\":6}," +
                 "\"tag\":{\"name\":\"Bil\",\"id\":5},\"id\":0}]";
-        Response response = POST("/t/save", "application/json", json);
+        Response response = POST("/transactions", "application/json", json);
         assertIsOk(response);
         assertContentType("application/json", response);
         assertCharset("utf-8", response);
@@ -84,7 +66,7 @@ public class RouteTest extends FunctionalTest {
                 "\"text\":\"456997107150**** 09.04 SEK 1550,00 CLAS OHLSON AB (49)\",\"archiveRef\":\"50001685147\"," +
                 "\"internal\":false,\"timestamp\":1239746400000,\"dirty\":true,\"type\":{\"name\":\"Visa\",\"id\":1}," +
                 "\"tag\":{\"name\":\"Bil\",\"id\":4},\"id\":7}]";
-        Response response = POST("/t/save", "application/json", json);
+        Response response = POST("/transactions", "application/json", json);
         assertIsOk(response);
         assertContentType("application/json", response);
         assertCharset("utf-8", response);
@@ -102,7 +84,7 @@ public class RouteTest extends FunctionalTest {
                 "\"text\":\"456997107150**** 09.04 SEK 1550,00 CLAS OHLSON AB (49)\",\"archiveRef\":\"50001685147\"," +
                 "\"internal\":false,\"timestamp\":1239746400000,\"dirty\":false,\"type\":{\"name\":\"Visa\",\"id\":1}," +
                 "\"tag\":{\"name\":\"Dagligvarer\",\"id\":4},\"id\":7}]";
-        Response response = POST("/t/save", "application/json", json);
+        Response response = POST("/transactions", "application/json", json);
         assertIsOk(response);
         assertContentType("application/json", response);
         assertCharset("utf-8", response);
