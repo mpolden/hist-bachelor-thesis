7905b5cb34bdf7869727682a5eadc96289974891
Martin Polden <martin.polden@gmail.com>
2011-03-21 11:28:45 +0100

    Added method for stream reading and refactored out HTTP methods

--- code/android/src/main/java/no/kantega/android/utils/GsonUtil.java
+++ code/android/src/main/java/no/kantega/android/utils/GsonUtil.java
@@ -3,46 +3,85 @@ package no.kantega.android.utils;
 import android.util.Log;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import no.kantega.android.models.AggregatedTag;
 import no.kantega.android.models.AverageConsumption;
 import no.kantega.android.models.Transaction;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class GsonUtil {
 
     private static final String TAG = GsonUtil.class.getSimpleName();
-    private static final Gson gson = new Gson();
     private static final String PORTABLE_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final Gson gson;
+
+    static {
+        final GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            @Override
+            public Date deserialize(JsonElement json, Type typeOfT,
+                                    JsonDeserializationContext context)
+                    throws JsonParseException {
+                SimpleDateFormat format = new SimpleDateFormat(
+                        PORTABLE_DATE_FORMAT);
+                try {
+                    return format.parse(json.getAsJsonPrimitive().
+                            getAsString());
+                } catch (ParseException e) {
+                    return null;
+                }
+            }
+        });
+        gson = builder.create();
+    }
 
     /**
-     * Retrieve body of the given URL
+     * Parse transactions from an InputStream
      *
-     * @param url
-     * @return URL body
+     * @param in
+     * @return List of transactions
      */
-    public static String getBody(final String url) {
-        DefaultHttpClient httpClient = new DefaultHttpClient();
-        HttpGet method = new HttpGet(url);
-        String body = null;
+    public static List<Transaction> parseTransactionsFromStream(
+            final InputStream in) {
+        List<Transaction> transactions = new ArrayList<Transaction>();
         try {
-            HttpResponse response = httpClient.execute(method);
-            body = EntityUtils.toString(response.getEntity());
+            JsonReader reader = new JsonReader(new InputStreamReader(
+                    in, "UTF-8"));
+            reader.beginArray();
+            while (reader.hasNext()) {
+                Transaction t = gson.fromJson(reader,
+                        Transaction.class);
+                transactions.add(t);
+            }
+            reader.endArray();
+            reader.close();
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, "UnsupportedEncodingException", e);
         } catch (IOException e) {
-            Log.d(TAG, "IOException", e);
+            Log.e(TAG, "IOException", e);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                Log.e(TAG, "IOException", e);
+            }
         }
-        return body;
+        return transactions;
     }
 
     /**
@@ -73,7 +112,8 @@ public class GsonUtil {
      * @return JSON representation
      */
     public static String makeJSON(final Object o) {
-        return new GsonBuilder().setDateFormat(PORTABLE_DATE_FORMAT).create().toJson(o);
+        return new GsonBuilder().setDateFormat(PORTABLE_DATE_FORMAT).create().
+                toJson(o);
     }
 
     /**
@@ -105,23 +145,8 @@ public class GsonUtil {
      * @return List of transactions
      */
     public static List<Transaction> parseTransactions(final String json) {
-        final GsonBuilder builder = new GsonBuilder();
-        builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
-            @Override
-            public Date deserialize(JsonElement json, Type typeOfT,
-                                    JsonDeserializationContext context)
-                    throws JsonParseException {
-                SimpleDateFormat format = new SimpleDateFormat(PORTABLE_DATE_FORMAT);
-                try {
-                    return format.parse(json.getAsJsonPrimitive().
-                            getAsString());
-                } catch (ParseException e) {
-                    return null;
-                }
-            }
-        });
         final Type listType = new TypeToken<List<Transaction>>() {
         }.getType();
-        return builder.create().fromJson(json, listType);
+        return gson.fromJson(json, listType);
     }
 }
