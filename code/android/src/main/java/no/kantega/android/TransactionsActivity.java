2543303744d6faf078617324ac7ffc32926f9d4b
Martin Polden <martin.polden@gmail.com>
2011-03-21 14:24:08 +0100

    Refactoring

--- code/android/src/main/java/no/kantega/android/TransactionsActivity.java
+++ code/android/src/main/java/no/kantega/android/TransactionsActivity.java
@@ -4,15 +4,18 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteCursor;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.*;
-import android.widget.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import no.kantega.android.controllers.Transactions;
 import no.kantega.android.models.Transaction;
-import no.kantega.android.models.TransactionTag;
 import no.kantega.android.utils.FmtUtil;
 
 import java.util.Date;
@@ -20,26 +23,18 @@ import java.util.Date;
 public class TransactionsActivity extends ListActivity {
 
     private static final String TAG = OverviewActivity.class.getSimpleName();
-
     private Transactions db;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transactions);
-
         db = new Transactions(getApplicationContext());
         Cursor transactionsCursor = db.getCursor();
         startManagingCursor(transactionsCursor);
         String[] from = {"accountingDate", "text", "tag", "amountOut"};
         int[] to = {R.id.trow_tv_date, R.id.trow_tv_text, R.id.trow_tv_category, R.id.trow_tv_amount};
         this.setListAdapter(new TransactionsAdapter(this, R.layout.transactionrow, transactionsCursor, from, to));
-        /*if (transactionsCursor.moveToFirst()) {
-            do {
-                String text = transactionsCursor.getString(transactionsCursor.getColumnIndex("text"));
-            } while (transactionsCursor.moveToNext());
-        }*/
-
     }
 
     @Override
@@ -64,72 +59,42 @@ public class TransactionsActivity extends ListActivity {
             }
             intent.putExtra("transaction", t);
             startActivity(intent);
-
         }
     }
 
     private class TransactionsAdapter extends SimpleCursorAdapter {
-        private Cursor cursor;
-        private Context context;
+
         private int layout;
 
-        public TransactionsAdapter(Context context, int layout, Cursor cursor, String[] from, int[] to) {
+        public TransactionsAdapter(Context context, int layout, Cursor cursor,
+                                   String[] from, int[] to) {
             super(context, layout, cursor, from, to);
-            this.context = context;
             this.layout = layout;
         }
 
-
+        @Override
         public View newView(Context context, Cursor cursor, ViewGroup parent) {
-
-            Cursor c = getCursor();
-
             final LayoutInflater inflater = LayoutInflater.from(context);
-            View v = inflater.inflate(layout, parent, false);
-
-            String date = c.getString(c.getColumnIndex("accountingDate"));
-            String text = c.getString(c.getColumnIndex("text"));
-            String tag = c.getString(c.getColumnIndex("tag"));
-            String amount = c.getString(c.getColumnIndex("amountOut"));
-
-            ImageView image = (ImageView) v.findViewById(R.id.tag_icon);
-            TextView tv_date = (TextView) v.findViewById(R.id.trow_tv_date);
-            TextView tv_text = (TextView) v.findViewById(R.id.trow_tv_text);
-            TextView tv_tag = (TextView) v.findViewById(R.id.trow_tv_category);
-            TextView tv_amount = (TextView) v.findViewById(R.id.trow_tv_amount);
-
-            if (tv_date != null) {
-                Date d = FmtUtil.stringToDate("yyyy-MM-dd HH:mm:ss", date);
-                tv_date.setText(FmtUtil.dateToString("yyyy-MM-dd", d));
-            }
-            if (text != null) {
-                tv_text.setText(FmtUtil.trimTransactionText(text));
-            }
-            if (tag != null) {
-                tv_tag.setText(tag);
-                image.setImageDrawable(getImageIdByTag(tag));
-            }
-            if (amount != null) {
-                tv_amount.setText(amount);
-            }
-
+            final View v = inflater.inflate(layout, parent, false);
+            populateView(v, cursor);
             return v;
-
         }
 
         @Override
         public void bindView(View v, Context context, Cursor c) {
+            populateView(v, c);
+        }
+
+        private void populateView(View v, Cursor c) {
             String date = c.getString(c.getColumnIndex("accountingDate"));
             String text = c.getString(c.getColumnIndex("text"));
             String tag = c.getString(c.getColumnIndex("tag"));
             String amount = c.getString(c.getColumnIndex("amountOut"));
-
             ImageView image = (ImageView) v.findViewById(R.id.tag_icon);
             TextView tv_date = (TextView) v.findViewById(R.id.trow_tv_date);
             TextView tv_text = (TextView) v.findViewById(R.id.trow_tv_text);
             TextView tv_tag = (TextView) v.findViewById(R.id.trow_tv_category);
             TextView tv_amount = (TextView) v.findViewById(R.id.trow_tv_amount);
-
             if (tv_date != null) {
                 Date d = FmtUtil.stringToDate("yyyy-MM-dd HH:mm:ss", date);
                 tv_date.setText(FmtUtil.dateToString("yyyy-MM-dd", d));
@@ -146,7 +111,6 @@ public class TransactionsActivity extends ListActivity {
             }
         }
     }
-
 
     private Drawable getImageIdByTag(String tag) {
         if ("Ferie".equals(tag)) {
@@ -175,5 +139,4 @@ public class TransactionsActivity extends ListActivity {
         super.onDestroy();
         db.close();
     }
-
 }
