57d4ecc95b433d80873cbaa8820a2b0ac7ad2d1d
Martin Polden <martin.polden@gmail.com>
2011-03-22 12:33:03 +0100

    Added an urlEncode method

--- code/android/src/main/java/no/kantega/android/utils/FmtUtil.java
+++ code/android/src/main/java/no/kantega/android/utils/FmtUtil.java
@@ -1,5 +1,7 @@
 package no.kantega.android.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -53,6 +55,20 @@ public class FmtUtil {
     public static String trimTransactionText(String text) {
         final String pattern = "(\\d+\\*+\\s)?(\\d{2}\\.\\d{2}\\s)?([A-Z]{3}\\s\\d+,\\d+\\s)?(TIL\\:\\s)?";
         return text == null ? "" : text.replaceAll(pattern, "");
+    }
+
+    /**
+     * URL encode the given string using UTF-8
+     *
+     * @param s
+     * @return The URL encoded string
+     */
+    public static String urlEncode(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
     }
 
     /**
