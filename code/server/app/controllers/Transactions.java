f323ca2d175a3bfc6aa36786a455f3ba7ea15c67
Martin Polden <martin.polden@gmail.com>
2011-03-23 14:02:05 +0100

    Removed deprecated controller methods

--- code/server/app/controllers/Transactions.java
+++ code/server/app/controllers/Transactions.java
@@ -1,73 +1,21 @@
 package controllers;
 
 import com.google.gson.JsonArray;
-import models.AggregatedTag;
-import models.AverageConsumption;
 import models.Transaction;
 import models.User;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import play.db.jpa.JPA;
 import play.mvc.Controller;
 import utils.GsonUtil;
 import utils.ModelHelper;
 
-import javax.persistence.Query;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Transactions extends Controller {
 
     private static Logger logger = Logger.getLogger(
             Transactions.class.getName());
-
-    @SuppressWarnings("unchecked")
-    @Deprecated
-    public static void topTags(int count) {
-        Query query = JPA.em().createQuery(
-                "select tag.name, sum(t.amountOut) from Transaction t" +
-                        " join t.tag as tag" +
-                        " group by tag.name order by sum(t.amountOut) desc").
-                setMaxResults(count);
-        List<Object[]> result = query.getResultList();
-        List<AggregatedTag> tags = new ArrayList<AggregatedTag>();
-        for (Object[] o : result) {
-            if (o.length != 2) {
-                throw new IllegalArgumentException(
-                        "Incorrect number of fields fetched");
-            }
-            AggregatedTag tag = new AggregatedTag();
-            tag.setName(o[0].toString());
-            tag.setAmount(Double.parseDouble(o[1].toString()));
-            tags.add(tag);
-        }
-        renderJSON(tags);
-    }
-
-    @Deprecated
-    private static Double avgDay() {
-        Transaction t;
-        t = Transaction.find("order by accountingDate asc").first();
-        Date start = t.accountingDate;
-        t = Transaction.find("order by accountingDate desc").first();
-        Date stop = t.accountingDate;
-        final int days =
-                (int) ((stop.getTime() - start.getTime()) / 1000) / 86400;
-        Query query = JPA.em().createQuery(
-                "select sum(t.amountOut) from Transaction t");
-        Object o = query.getSingleResult();
-        return Double.parseDouble(o.toString()) / days;
-    }
-
-    @Deprecated
-    public static void avg() {
-        AverageConsumption ac = new AverageConsumption();
-        ac.setDay(avgDay());
-        ac.setWeek(avgDay() * 7);
-        ac.setMonth(avgDay() * 30.4368499); // Google approves
-        renderJSON(ac);
-    }
 
     public static void all(String registrationId) {
         final List<Transaction> transactions = Transaction.
