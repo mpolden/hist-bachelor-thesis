e77f67bf2a5743b0e22f572bce6d1353e3e3bdb4
Martin Polden <martin.polden@gmail.com>
2011-03-23 14:55:31 +0100

    Added method for parsing from string

--- code/server/app/utils/GsonUtil.java
+++ code/server/app/utils/GsonUtil.java
@@ -13,13 +13,9 @@ import java.util.List;
 public class GsonUtil {
 
     private static final String PORTABLE_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final GsonBuilder builder = new GsonBuilder();
 
-    public static String makeJSON(Object o) {
-        return new GsonBuilder().setDateFormat(PORTABLE_DATE_FORMAT).create().toJson(o);
-    }
-
-    public static List<Transaction> parseTransactions(JsonArray jsonArray) {
-        final GsonBuilder builder = new GsonBuilder();
+    static {
         builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
             @Override
             public Date deserialize(JsonElement json, Type typeOfT,
@@ -34,6 +30,19 @@ public class GsonUtil {
                 }
             }
         });
+    }
+
+    public static String makeJSON(Object o) {
+        return new GsonBuilder().setDateFormat(PORTABLE_DATE_FORMAT).create().toJson(o);
+    }
+
+    public static List<Transaction> parseTransactions(String s) {
+        final Type listType = new TypeToken<List<Transaction>>() {
+        }.getType();
+        return builder.create().fromJson(s, listType);
+    }
+
+    public static List<Transaction> parseTransactions(JsonArray jsonArray) {
         final Type listType = new TypeToken<List<Transaction>>() {
         }.getType();
         return builder.create().fromJson(jsonArray, listType);
