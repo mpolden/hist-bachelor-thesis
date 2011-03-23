63d6d0815326b3a74255dbab36f5fe98857a7ddb
Martin Polden <martin.polden@gmail.com>
2011-03-23 19:09:52 +0100

    Removed some deprecated methods

--- code/android/src/main/java/no/kantega/android/afp/utils/HttpUtil.java
+++ code/android/src/main/java/no/kantega/android/afp/utils/HttpUtil.java
@@ -4,7 +4,6 @@ import android.util.Log;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -13,72 +12,11 @@ import org.apache.http.util.EntityUtils;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 public class HttpUtil {
 
     private static final String TAG = GsonUtil.class.getSimpleName();
-
-    /**
-     * Retrieve body of the given URL
-     *
-     * @param url
-     * @return URL body
-     */
-    @Deprecated
-    public static String getBody(final String url) {
-        DefaultHttpClient httpClient = new DefaultHttpClient();
-        HttpGet method = new HttpGet(url);
-        String body = null;
-        try {
-            HttpResponse response = httpClient.execute(method);
-            body = EntityUtils.toString(response.getEntity());
-        } catch (IOException e) {
-            Log.d(TAG, "IOException", e);
-        }
-        return body;
-    }
-
-    /**
-     * Retrieve body of the given URL as an InputStream (for large requests)
-     *
-     * @param url
-     * @return Body as InputStream
-     */
-    @Deprecated
-    public static InputStream getBodyAsStream(final String url, Map<String, String> params) {
-        try {
-            URL u = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) u.
-                    openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setChunkedStreamingMode(0);
-            InputStream out = new BufferedInputStream(connection.getInputStream());
-            return out;
-        } catch (MalformedURLException e) {
-            Log.d(TAG, "IOException", e);
-        } catch (IOException e) {
-            Log.d(TAG, "IOException", e);
-        }
-        return null;
-    }
-
-    /**
-     * Post JSON to URL
-     *
-     * @param url
-     * @param json
-     */
-    @Deprecated
-    public static String postJSON(final String url, final String json) {
-        return post(url, json, "application/json");
-    }
 
     public static String post(final String url, final String s) {
         return post(url, s, "text/plain");
@@ -91,8 +29,6 @@ public class HttpUtil {
             method.setEntity(new UrlEncodedFormEntity(values));
             HttpResponse response = httpClient.execute(method);
             return new BufferedInputStream(response.getEntity().getContent());
-            /*response.getEntity().getContent();
-            body = EntityUtils.toString(response.getEntity());*/
         } catch (IOException e) {
             Log.e(TAG, "IOException", e);
         }
