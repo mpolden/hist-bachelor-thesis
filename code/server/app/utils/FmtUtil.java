cae05fc23dfd657953442cba377211280d3dc1d8
Martin Polden <martin.polden@gmail.com>
2011-03-23 15:15:29 +0100

    Added method for encoding a map of form parameters

--- code/server/app/utils/FmtUtil.java
+++ code/server/app/utils/FmtUtil.java
@@ -1,5 +1,9 @@
 package utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
 public class FmtUtil {
 
     /**
@@ -11,5 +15,24 @@ public class FmtUtil {
     public static String trimTransactionText(String text) {
         final String pattern = "(\\d+\\*+\\s)?(\\d{2}\\.\\d{2}\\s)?([A-Z]{3}\\s\\d+,\\d+\\s)?(TIL\\:\\s)?";
         return text == null ? "" : text.replaceAll(pattern, "");
+    }
+
+    public static String encode(Map<String, String> params) {
+        final StringBuilder body = new StringBuilder();
+        boolean first = true;
+        for (String key : params.keySet()) {
+            if (!first) {
+                body.append("&");
+            }
+            body.append(key);
+            body.append("=");
+            try {
+                body.append(URLEncoder.encode(params.get(key), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                body.append("");
+            }
+            first = false;
+        }
+        return body.toString();
     }
 }
