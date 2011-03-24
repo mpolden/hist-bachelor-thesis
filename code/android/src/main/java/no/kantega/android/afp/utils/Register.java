f14f2c053f84450ef3eed700ad8d961223850a48
Martin Polden <martin.polden@gmail.com>
2011-03-24 14:35:56 +0100

    Log deviceId

--- code/android/src/main/java/no/kantega/android/afp/utils/Register.java
+++ code/android/src/main/java/no/kantega/android/afp/utils/Register.java
@@ -2,10 +2,12 @@ package no.kantega.android.afp.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 import com.google.android.c2dm.C2DMessaging;
 
 public class Register {
 
+    private static final String TAG = Register.class.getSimpleName();
     public static final String SENDER_ID = "androidafp@gmail.com";
     public static final String REGISTRATION_ID_KEY = "deviceRegistrationID";
 
@@ -16,9 +18,11 @@ public class Register {
      */
     public static void handleRegistration(Context context) {
         SharedPreferences preferences = Prefs.get(context);
-        if (preferences.getString(REGISTRATION_ID_KEY, null) == null) {
+        String deviceId = preferences.getString(REGISTRATION_ID_KEY, null);
+        if (deviceId == null) {
             C2DMessaging.register(context, SENDER_ID);
         }
+        Log.d(TAG, "Existing Device ID: " + deviceId);
     }
 
     /**
