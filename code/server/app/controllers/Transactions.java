04aec8dee23abcf96716d0709d0efbfe455a3883
Martin Polden <martin.polden@gmail.com>
2011-03-24 12:39:47 +0100

    Refactor

--- code/server/app/controllers/Transactions.java
+++ code/server/app/controllers/Transactions.java
@@ -37,49 +37,16 @@ public class Transactions extends Controller {
 
     public static void save(String registrationId, JsonArray json) {
         final List<Transaction> transactions = GsonUtil.parseTransactions(json);
+        final List<Transaction> updated = new ArrayList<Transaction>();
         final User user = User.find("deviceId", registrationId).first();
         if (user != null) {
-            List<Transaction> updated = saveTransactions(transactions, user);
+            for (Transaction t : transactions) {
+                updated.add(ModelHelper.saveTransaction(t, user));
+            }
             renderJSON(GsonUtil.makeJSON(updated));
         } else {
             logger.log(Level.WARN, "Could not find user with registrationId: " +
                     registrationId);
         }
-    }
-
-    private static List<Transaction> saveTransactions(
-            List<Transaction> transactions, User user) {
-        final List<Transaction> updated = new ArrayList<Transaction>();
-        for (Transaction t : transactions) {
-            if (t.dirty) {
-                Transaction existing = Transaction.findById(t.id);
-                if (existing != null) {
-                    existing._id = t._id;
-                    existing.accountingDate = t.accountingDate;
-                    existing.amountIn = t.amountIn;
-                    existing.amountOut = t.amountOut;
-                    existing.text = t.text;
-                    existing.internal = t.internal;
-                    existing.timestamp = t.timestamp;
-                    existing.dirty = false;
-                    existing.tag = ModelHelper.getOrSaveTag(t.tag.name);
-                    existing.type = ModelHelper.getOrAddType(t.type.name);
-                    existing.save();
-                    updated.add(existing);
-                } else {
-                    t.id = null;
-                    t.tag = ModelHelper.getOrSaveTag(t.tag.name);
-                    t.type = ModelHelper.getOrAddType(t.type.name);
-                    t.dirty = false;
-                    t.user = user;
-                    t.save();
-                    updated.add(t);
-                }
-            } else {
-                logger.log(Level.WARN,
-                        "Not saving non-dirty transaction with _id: " + t._id);
-            }
-        }
-        return updated;
     }
 }
