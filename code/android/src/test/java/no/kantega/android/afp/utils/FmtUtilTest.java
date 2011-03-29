1d5b7ae34e4d1b5c7d262ccb9c9e57847bfa21f6
Martin Polden <martin.polden@gmail.com>
2011-03-29 11:04:21 +0200

    Updated trimTransactionText and test

--- code/android/src/test/java/no/kantega/android/afp/utils/FmtUtilTest.java
+++ code/android/src/test/java/no/kantega/android/afp/utils/FmtUtilTest.java
@@ -38,11 +38,11 @@ public class FmtUtilTest {
         String actual;
         expected = "STATOIL INNHERREDSVEIE";
         actual = FmtUtil.trimTransactionText(
-                "456997107150**** 29.03 NOK 500,37 STATOIL INNHERREDSVEIE");
+                "4569971071500000 29.03 NOK 500,37 STATOIL INNHERREDSVEIE");
         assertEquals(expected, actual);
         expected = "JACK & JONES OSTERSUND";
         actual = FmtUtil.trimTransactionText(
-                "456997107150**** 08.04 SEK 1180,00 JACK & JONES OSTERSUND");
+                "4569971071500000 08.04 SEK 1180,00 JACK & JONES OSTERSUND");
         assertEquals(expected, actual);
         expected = "JACK & JONES OSTERSUND";
         actual = FmtUtil.trimTransactionText(
@@ -52,9 +52,12 @@ public class FmtUtilTest {
         actual = FmtUtil.trimTransactionText(
                 "27.03 NIDAR AS BLOMSTADVN 2 TRONDHEIM");
         assertEquals(expected, actual);
-        expected = "SATS Norge AS                      BETNR:       279";
+        expected = "SATS Norge AS";
         actual = FmtUtil.trimTransactionText(
                 "TIL: SATS Norge AS                      BETNR:       279");
+        assertEquals(expected, actual);
+        expected = "REMA 1000 MELHUS";
+        actual = FmtUtil.trimTransactionText("REMA 1000 MELHUS");
         assertEquals(expected, actual);
         expected = "";
         actual = FmtUtil.trimTransactionText(null);
