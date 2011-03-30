a31d0bd747ba1ea90e5aedcc6f3a355e337fdacc
Martin Polden <martin.polden@gmail.com>
2011-03-30 14:18:15 +0200

    Added a resource helper class

---
+++ code/android/src/main/java/no/kantega/android/afp/utils/ResourceHelper.java
@@ -0,0 +1,35 @@
+package no.kantega.android.afp.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import no.kantega.android.afp.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceHelper extends Activity {
+
+    private static final Map<String, Integer> imageIds;
+
+    static {
+        imageIds = new HashMap<String, Integer>() {{
+            put("Annet", R.drawable.tag_creditcard);
+            put("Barn", R.drawable.tag_baby);
+            put("Ferie", R.drawable.tag_sun);
+            put("Fornøyelser", R.drawable.tag_controller);
+            put("Helse", R.drawable.tag_medicalbag);
+            put("Husholdning", R.drawable.tag_house);
+            put("Klær", R.drawable.tag_tshirt);
+            put("Mat", R.drawable.tag_forkandknife);
+            put("Transport", R.drawable.tag_airplane);
+            put("Sparing", R.drawable.tag_linechart);
+
+        }};
+    }
+
+    public static Drawable getImage(Context context, String tag) {
+        return context.getResources().getDrawable(imageIds.get(tag));
+    }
+
+}
