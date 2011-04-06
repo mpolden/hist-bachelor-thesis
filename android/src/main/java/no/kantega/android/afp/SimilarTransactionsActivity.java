9fa5d3c0a637a791bd7f92e2a078fe708ce860c9
Martin Polden <martin.polden@gmail.com>
2011-04-06 12:38:23 +0200

    Added an activity for similar transactions

---
+++ android/src/main/java/no/kantega/android/afp/SimilarTransactionsActivity.java
@@ -0,0 +1,14 @@
+package no.kantega.android.afp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * This activity handles similar transactions
+ */
+public class SimilarTransactionsActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
