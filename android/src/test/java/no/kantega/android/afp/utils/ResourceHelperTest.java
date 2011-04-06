6f7ae4c9bf62fa32329b818e475a3f9a5f492025
Martin Polden <martin.polden@gmail.com>
2011-04-06 18:38:25 +0200

    Added test for ResourceHelper

---
+++ android/src/test/java/no/kantega/android/afp/utils/ResourceHelperTest.java
@@ -0,0 +1,21 @@
+package no.kantega.android.afp.utils;
+
+import android.content.Context;
+import no.kantega.android.afp.MavenizedTestRunner;
+import no.kantega.android.afp.OverviewActivity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(MavenizedTestRunner.class)
+public class ResourceHelperTest {
+
+    @Test
+    public void testGetImage() {
+        final Context context = new OverviewActivity().getApplicationContext();
+        assertNotNull(ResourceHelper.getImage(context, "tag"));
+        assertNotNull(ResourceHelper.getImage(context, null));
+    }
+
+}
