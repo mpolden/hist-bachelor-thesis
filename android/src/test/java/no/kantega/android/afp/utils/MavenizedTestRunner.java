3279135e1721f0da3293ad4c3228eb33285f75d3
Martin Polden <martin.polden@gmail.com>
2011-04-06 14:23:28 +0200

    Added a test runner for Android-specific tests

---
+++ android/src/test/java/no/kantega/android/afp/utils/MavenizedTestRunner.java
@@ -0,0 +1,24 @@
+package no.kantega.android.afp.utils;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.runners.model.InitializationError;
+
+import java.io.File;
+
+/**
+ * Custom test runner that specifies the correct path to AndroidManifest.xml and res directory
+ */
+public class MavenizedTestRunner extends RobolectricTestRunner {
+
+    /**
+     * Configure test runner
+     *
+     * @param testClass Class to test
+     * @throws InitializationError Thrown if path doesn't contain required files
+     */
+    public MavenizedTestRunner(Class testClass) throws InitializationError {
+        super(testClass, new File("./android/src/main/android"));
+    }
+}
+
+
