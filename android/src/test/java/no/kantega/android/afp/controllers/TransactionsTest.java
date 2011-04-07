ca11b572771d06bf665976674275bf1da3fb008b
Martin Polden <martin.polden@gmail.com>
2011-04-07 15:30:43 +0200

    Attempt at implementing ShadowSQLiteDatabase

--- android/src/test/java/no/kantega/android/afp/controllers/TransactionsTest.java
+++ android/src/test/java/no/kantega/android/afp/controllers/TransactionsTest.java
@@ -2,27 +2,31 @@ package no.kantega.android.afp.controllers;
 
 import no.kantega.android.afp.MavenizedTestRunner;
 import no.kantega.android.afp.OverviewActivity;
+import no.kantega.android.afp.models.Transaction;
+import no.kantega.android.afp.models.TransactionTag;
 import no.kantega.android.afp.utils.DatabaseHelper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+
 @RunWith(MavenizedTestRunner.class)
 public class TransactionsTest {
 
-    private Transactions transactions;
-
     @Before
     public void setUp() {
-        OverviewActivity activity = new OverviewActivity();
-        this.transactions = new Transactions(activity.getApplicationContext(),
-                new DatabaseHelper(activity.getApplicationContext(), null, 1));
     }
 
     @Test
     public void testAdd() {
+        OverviewActivity activity = new OverviewActivity();
+        Transactions transactions = new Transactions(activity.getApplicationContext(),
+                new DatabaseHelper(activity.getApplicationContext(), null, 1));
         // XXX: Not working until Robolectra implements android.database.sqlite.SQLiteDatabase.rawQuery
-        /*final Transaction t = new Transaction();
+        final Transaction t = new Transaction();
         t.setDate(new Date());
         t.setAmount(133.7);
         t.setText("Test");
@@ -35,7 +39,6 @@ public class TransactionsTest {
         t.setInternal(false);
         t.setDirty(true);
         transactions.add(t);
-        assertTrue(transactions.getCount() == 1);*/
+        assertTrue(transactions.getCount() == 1);
     }
-
 }
