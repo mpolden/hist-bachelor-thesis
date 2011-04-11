9d8c615b56abe7237b4d9defbdb8dcd6af48eb7d
Martin Polden <martin.polden@gmail.com>
2011-04-11 16:13:04 +0200

    Table name 'user' is reserved in PostgreSQL

--- server/app/models/User.java
+++ server/app/models/User.java
@@ -4,11 +4,13 @@ import play.db.jpa.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * This model class represents a user
  */
 @Entity
+@Table(name = "users")
 public class User extends Model {
 
     // XXX: Only deviceId for now, probably needs username/password since
