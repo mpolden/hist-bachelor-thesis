6db62ccfc28cb24b4974f1022c8b1d632f1f4eb9
Vegard Johannessen <vegardoj@stud.hist.no>
2011-04-11 14:38:53 +0200

    Added an input field to settings for username. Also added settings to the synchronizeactivity menu.

--- android/src/main/java/no/kantega/android/afp/SettingsActivity.java
+++ android/src/main/java/no/kantega/android/afp/SettingsActivity.java
@@ -2,12 +2,17 @@ package no.kantega.android.afp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SettingsActivity extends Activity {
+    EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.settings);
+        this.username = (EditText) findViewById(R.id.et_username);
     }
+
+
 }
