051b800887516a11c224a4064841e87f26b99b4c
Martin Polden <martin.polden@gmail.com>
2011-04-11 15:37:17 +0200

    Call registerWithServer on save

--- android/src/main/java/no/kantega/android/afp/SettingsActivity.java
+++ android/src/main/java/no/kantega/android/afp/SettingsActivity.java
@@ -3,39 +3,35 @@ package no.kantega.android.afp;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import no.kantega.android.afp.utils.Prefs;
+import no.kantega.android.afp.utils.Register;
 
 public class SettingsActivity extends Activity {
+
     private EditText inputUsername;
-    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-
         this.inputUsername = (EditText) findViewById(R.id.et_username);
-        this.save = (Button) findViewById(R.id.button_settings_save);
-
-        SharedPreferences app_preferences =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        String username = app_preferences.getString("username", "");
+        Button save = (Button) findViewById(R.id.button_settings_save);
+        final SharedPreferences preferences = Prefs.get(getApplicationContext());
+        final String username = preferences.getString("username", null);
         inputUsername.setText(username);
-
-        final SharedPreferences.Editor editor = app_preferences.edit();
+        final SharedPreferences.Editor editor = preferences.edit();
         save.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         editor.putString("username", inputUsername.getText().toString());
                         editor.commit();
+                        Register.registerWithServer(getApplicationContext());
                         finish();
                     }
                 });
     }
-
-
 }
