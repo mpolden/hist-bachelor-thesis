abdd77d54a4343ce9954c274ad95da4605d6b1d2
Martin Polden <martin.polden@gmail.com>
2011-04-11 15:08:31 +0200

    Implemented registerWithServer

--- android/src/main/java/no/kantega/android/afp/utils/Register.java
+++ android/src/main/java/no/kantega/android/afp/utils/Register.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 import com.google.android.c2dm.C2DMessaging;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.Properties;
 
 /**
  * This class provides helper methods for C2DM registration
@@ -13,6 +18,7 @@ public class Register {
     private static final String TAG = Register.class.getSimpleName();
     public static final String SENDER_ID = "androidafp@gmail.com";
     public static final String REGISTRATION_ID_KEY = "deviceRegistrationID";
+    public static final String USERNAME_KEY = "username";
 
     /**
      * Register the device if it doesn't already have registration ID
@@ -31,18 +37,24 @@ public class Register {
     /**
      * Register with server
      *
-     * @param registrationId Registration ID for this device
+     * @param context Application context
      */
-    public static void registerWithServer(String registrationId) {
-        throw new RuntimeException("Stub!");
-    }
-
-    /**
-     * Unregister from server
-     *
-     * @param registrationId Registration ID for this device
-     */
-    public static void unregisterFromServer(String registrationId) {
-        throw new RuntimeException("Stub!");
+    public static void registerWithServer(Context context) {
+        final SharedPreferences preferences = Prefs.get(context);
+        final String deviceId = preferences.getString(REGISTRATION_ID_KEY, null);
+        final String username = preferences.getString(USERNAME_KEY, null);
+        final Properties properties = Prefs.getProperties(context);
+        if (deviceId != null && username != null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    HttpUtil.post(properties.getProperty("register").toString(),
+                            new ArrayList<NameValuePair>() {{
+                                add(new BasicNameValuePair("username", username));
+                                add(new BasicNameValuePair("deviceId", deviceId));
+                            }});
+                }
+            }).start();
+        }
     }
 }
