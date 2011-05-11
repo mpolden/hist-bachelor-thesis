acfa406e666cfc2fbeda1343554af090d60859ca
Martin Polden <martin.polden@gmail.com>
2011-05-11 13:20:41 +0200

    Check if response object is null

--- android/src/main/java/no/kantega/android/afp/utils/Register.java
+++ android/src/main/java/no/kantega/android/afp/utils/Register.java
@@ -55,7 +55,7 @@ public class Register {
                                 add(new BasicNameValuePair("username", username));
                                 add(new BasicNameValuePair("registrationId", deviceId));
                             }});
-                    if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                    if (response == null || response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
                         Log.e(TAG, "Failed to register wither server");
                     }
                     Log.d(TAG, String.format("Registered %s with device ID: %s", username, deviceId));
